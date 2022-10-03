4acf706cb4114476224d4d9e4ed5467b148d0b6a
dciro8 <inforfercho@gmail.com>
2022-10-03 15:52:59 -0500

    US1: Refactor  caso de uso

--- betting.soccer.scores.api/Domains/SoccerGameService/SoccerGamePage/EnumStateGame.cs
+++ betting.soccer.scores.api/Domains/SoccerGameService/SoccerGamePage/EnumStateGame.cs
@@ -5,8 +5,8 @@ namespace betting.soccer.scores.api.Domains.SoccerGameService.SoccerGamePage
     public enum EnumStateGame
     {
         [Display(Description = "Programer")] P,
-        [Display(Description = "G")] Game,
-        [Display(Description = "F")] Finish,
-        [Display(Description = "C")] Cancel
+        [Display(Description = "Game")]G,
+        [Display(Description = "Finish")] F,
+        [Display(Description = "Cancel")] C
     }
 }
